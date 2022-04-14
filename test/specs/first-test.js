134453ce6648cdbda37c8af0a865d4404c919349
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-04-14 21:13:28 +0200

    waitUntil

--- test/specs/first-test.js
+++ test/specs/first-test.js
@@ -1,24 +1,37 @@
-describe('Ecommerce App', async() => {
-  
-  it('Login fail page', async() => {
+describe('Ecommerce App', async () => {
+  it('Login fail page', async () => {
     // open browser
-    await browser.url('https://www.rahulshettyacademy.com/loginpagePractise/');
+    await browser.url(
+      'https://www.rahulshettyacademy.com/loginpagePractise/'
+    );
     // await expect(browser).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
     // await expect(browser).toHaveTitle('LoginPage Praaaactise | Rahul Shetty Academy');
     // css selector #id .className, !!! tagname[attribute='value'] / input[neame='username'], !!! xpath
-    await $("input[name='username']").setValue("rahulshettyacademy");
+    await $("input[name='username']").setValue(
+      'rahulshettyacademy'
+    );
     // await browser.pause(3000);
-    await $('#username').setValue("secondCSS");
+    await $('#username').setValue('secondCSS');
     // await browser.pause(3000);
     // XPath : //tagname[@attribute='value']
     // await $("//input[@id='password']").setValue('password');
     const password = $("//input[@id='password']");
     await password.setValue('learningsad');
     await $('#signInBtn').click();
-    await browser.pause(3000);
-    await console.log(await $('.alert-danger').getText());
+    await browser.waitUntil(
+      async () =>
+        (await $('#signInBtn').getAttribute(
+          'value'
+        )) === 'Sign In',
+      {
+        timeout: 5000,
+        timeoutMsg: 'Error msg is not showing up',
+      }
+    );
+    await console.log(
+      await $('.alert-danger').getText()
+    );
     await browser.pause(3000);
 
-    
   });
 });
