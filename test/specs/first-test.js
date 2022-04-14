f0064e26809b88a48688c53c56a088beea6bd858
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-04-14 20:31:59 +0200

    selectors and pause

--- test/specs/first-test.js
+++ test/specs/first-test.js
@@ -3,7 +3,15 @@ describe('Ecommerce App', async() => {
   it('Login fail page', async() => {
     // open browser
     await browser.url('https://www.rahulshettyacademy.com/loginpagePractise/');
-    console.log(await browser.getTitle());
+    // await expect(browser).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
+    // await expect(browser).toHaveTitle('LoginPage Praaaactise | Rahul Shetty Academy');
+    // css selector #id .className, !!! tagname[attribute='value'] / input[neame='username'], !!! xpath
+    await $("input[name='username']").setValue("rahulshettyacademy");
+    await browser.pause(3000);
+    await $('#username').setValue("secondCSS");
+    await browser.pause(3000);
+
+
     
   });
 });
