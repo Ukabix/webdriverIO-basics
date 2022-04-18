9f96accf621655f0fa51c2e536c6cbb9fc8ce947
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-04-18 18:10:01 +0200

    validate modal toBeDisplayed

--- test/specs/ui-controls.js
+++ test/specs/ui-controls.js
@@ -18,10 +18,25 @@ describe('UI Controls Test Suite', async () => {
     // const radioButtons = await $$(".radiotextsty");
     // const userDropdown = radioButtons[1];
     // await userDropdown.click();
+    // validate popup showing up
     // reach parent element - chain elements
     const radioButtons1 = await $$(".customradio");
     // span = child element
     await radioButtons1[1].$("span").click();
+    const waitForModal = await $(".modal-body");
+    await waitForModal.waitForDisplayed();
+    await $("#cancelBtn").click();
+    // query Admin radiobtn selection
+    console.log(await $$(".customradio")[0].$("span").isSelected());
+    //change radio selection
+    await radioButtons1[1].$("span").click();
+    await waitForModal.waitForDisplayed();
+    await $("#okayBtn").click();
+    // validate popup not showing up
+    await $$(".customradio")[0].$("span").click();
+    await expect(waitForModal).not.toBeDisplayed();
+
+
     await browser.pause(3000);
 
 
