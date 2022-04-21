8bab0e1ce8aaf8c5e11bacfddc92c33bc1b205e6
Ukabix <54454097+Ukabix@users.noreply.github.com>
2022-04-22 00:13:25 +0200

    formatting and cleanup

--- test/pageobjects/shop-page.js
+++ test/pageobjects/shop-page.js
@@ -1,19 +1,14 @@
 class ShopPage {
-
   // locators
-  
+
   // gets bind to object.property
   get btnCheckout() {
-    return $(
-      ".nav-link.btn.btn-primary"
-    );
-  };
+    return $('.nav-link.btn.btn-primary');
+  }
 
   get elementsCards() {
-    return $$(
-      "div[class='card h-100']"
-    );
-  };
+    return $$("div[class='card h-100']");
+  }
 
   async addProductsToCart(products) {
     // iterate elementsCards array and for each matching products -> add to cart
@@ -23,7 +18,9 @@ class ShopPage {
       i++
     ) {
       // get titles via DOM traversal to reach: div[class='card h-100'] div h4 a - chain locators
-      const card = await this.elementsCards[i].$("div h4 a");
+      const card = await this.elementsCards[i].$(
+        'div h4 a'
+      );
       // filter products via if statement, if true -> click that card
       if (
         products.includes(await card.getText())
@@ -31,13 +28,12 @@ class ShopPage {
         //// click that card
         // get locator for add btn
         const btn = await this.elementsCards[i].$(
-          ".card-footer button"
+          '.card-footer button'
         );
         await btn.click();
-      };
-    };
-  };
+      }
+    }
+  }
+}
 
-};
-
-module.exports = new ShopPage;
+module.exports = new ShopPage();
